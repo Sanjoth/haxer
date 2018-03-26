84d4ccf0a914491a9a58780327870cf523083af5
sanjoth <sanjoth.shaw@india.nec.com>
2018-03-26 19:24:50 +0530

    Flickity fix, Add more view elements

--- src/app/components/recommender/recommender.component.ts
+++ src/app/components/recommender/recommender.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, AfterViewInit, AfterViewChecked } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { forkJoin } from 'rxjs/observable/forkJoin'
 import * as flick from 'flickity';
 
 
@@ -8,46 +9,93 @@ import * as flick from 'flickity';
   templateUrl: './recommender.component.html',
   styleUrls: ['./recommender.component.css']
 })
-export class RecommenderComponent implements OnInit, AfterViewChecked {
-  trending = '';
+export class RecommenderComponent implements OnInit {
+  similar:any;
+  now_playing:any;
+  trending_now:any;
   data: any;
   iter = 0;
   flk:any;
+  flk1:any;
+  popular:any;
+  get_similar:any;
+  similar_last:any;
+
   constructor(private http: HttpClient) {
+    
   }
 
   ngOnInit() {
+    let obj:check = JSON.parse(localStorage.getItem("TRACKED_DATA"));
+    for (let id in obj)
+    {
+      this.similar_last = id;
+    }
+    console.log(this.similar_last);
     this.getData();
+    console.log(localStorage.getItem("TRACKED_DATA"));
+    
+    
+
   }
 
   getData() {
     let region = 'IN';
-    let lang = 'en-US'
-    this.trending = 'https://api.themoviedb.org/3/movie/popular?api_key=bd5e7f8161070f86bff1d8da34219f57&region=' + region + '&language=' + lang + '&page=1';
-    this.http.get<UserResponse>(this.trending).subscribe(data => {
-      this.data = data; // Assign local to global
-      console.log(data);
+    let lang = 'en-US';
+    let character = this.http.get<UserResponse>('https://api.themoviedb.org/3/movie/now_playing?api_key=bd5e7f8161070f86bff1d8da34219f57&language='+ lang +'&page=1');
+    let characterHomeworld = this.http.get<UserResponse>('https://api.themoviedb.org/3/movie/'+this.similar_last+'/similar?api_key=bd5e7f8161070f86bff1d8da34219f57&language='+ lang +'&page=1');
+    let trending = this.http.get<UserResponse>('https://api.themoviedb.org/3/movie/popular?api_key=bd5e7f8161070f86bff1d8da34219f57&region=' + region + '&language=' + lang + '&page=1');
+    
+    forkJoin([trending, character,characterHomeworld]).subscribe(data => {
+      // results[0] is our character
+      // results[1] is our character homeworld
+      this.data=data;
+      this.trending_now = data[0];
+      this.similar = data[2];
+      this.now_playing = data[1];
+      window.setTimeout(() => {this.abcd();});
     });
     
   }
 
-  ngAfterViewChecked() {
-      if (this.iter <= 3 && this.iter >= 2) {
+  abcd()
+  {
+    this.create_obj();
+  }
+
+  create_obj() {
+    try{
         let elem = document.querySelector('.carousel');
-        console.log(elem);
+        let elem2 = document.querySelector('.arry');
+        let elem3 = document.querySelector('.larry');
+        console.log("ELEMENT:" + elem);
        
+        this.flk = new flick(elem3, {
+          wrapAround: true,
+          groupCells: true,
+          cellAlign: 'left',
+          autoPlay: 5500
+        });
+
         this.flk = new flick(elem, {
           wrapAround: true,
           groupCells: true,
           cellAlign: 'left',
           autoPlay: 5500
         });
-  
-       // this.flk.resize();
+        
+        this.flk = new flick(elem2, {
+          wrapAround: true,
+          groupCells: true,
+          cellAlign: 'left',
+          autoPlay: 5500
+        });
+        console.log(this.flk);
       }
-      this.iter++;
+    catch(e){
+      console.log(e);
     }
-    
+}
 }
 
 interface UserResponse {
@@ -71,4 +119,8 @@ interface Results {
   popularity: number;
   poster_path: string;
   release_date: string;
+}
+
+interface check {
+  id: string;
 }
